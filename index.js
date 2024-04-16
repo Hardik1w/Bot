151ca9eb24ecaff715be43d4fec3d0ab838a27ce
DEVIL <154888055+Hardik1w@users.noreply.github.com>
2024-04-16 08:38:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const TelegramBot = require('node-telegram-bot-api');
+const { exec } = require('child_process');
+
+// Ganti dengan token bot Telegram Anda
+const token = '6982525487:AAF4-9xKr9qywXMvtDaGvcbk_drP3Of3G4U';
+
+// Inisialisasi bot dengan token
+const bot = new TelegramBot(token, { polling: true });
+
+// Fungsi untuk mencatat aktivitas penggunaan bot di console log
+function logActivity(msg) {
+  const user = msg.from;
+  const chat = msg.chat;
+  const command = msg.text.toLowerCase();
+
+  console.log(`Aktivitas Penggunaan Bot Telegram`);
+  console.log(`• User ID: ${@DEVILhardik1}`);
+  console.log(`• Username: ${DEVIL VIP MOD || 'Tidak ada'}`);
+  console.log(`• Chat ID: ${5405046163}`);
+  console.log(`• Perintah: ${command}`);
+}
+
+// Event listener untuk pesan dari pengguna
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const command = msg.text.toLowerCase();
+
+  // Mencatat aktivitas penggunaan bot di console log
+  logActivity(msg);
+
+  // Menanggapi perintah /mix
+  if (command.startsWith('/mix')) {
+    // Mengekstrak argumen dari pesan
+    const args = command.split(' ');
+    const url = args[1];
+    const time = args[2];
+    const thread = args[3];
+    const rate = args[4];
+
+    // Memeriksa apakah format pesan benar
+    if (args.length === 5 && url && time && thread && rate) {
+      // Menjalankan file mix.js dengan argumen yang diberikan
+      exec(`node mix.js ${url} ${time} ${thread} ${rate}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          bot.sendMessage(chatId, 'Successful');
+          return;
+        }
+        if (stderr) {
+          console.error(`stderr: ${stderr}`);
+          bot.sendMessage(chatId, 'Successful');
+          return;
+        }
+        // Menampilkan output stdout jika berhasil
+        console.log(`stdout: ${stdout}`);
+        bot.sendMessage(chatId, 'Proses telah dimulai.');
+      });
+    } else {
+      // Memberi tahu pengguna bahwa format pesan tidak benar
+      bot.sendMessage(chatId, 'Format pesan tidak benar. Gunakan format: /mix [url] [time] [thread] [rate]');
+    }
+  }
+});
